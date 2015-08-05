665eb6e83e986793710522f696f8d495f51bfbd2
Deepfilter <jose@deepfilter.org>
2015-08-05 18:35:39 -0500

    Update and rename jquery-deepfilter.js to liveSearch.js

---
+++ liveSearch.js
@@ -0,0 +1,15 @@
+$.fn.liveSearch = function(table) {
+$(this).on('keyup', function() {
+var val = $.trim($(this).val()).replace(/Ã¡/g, 'a').replace(/Ã©/g, 'e').replace(/Ã­/g, 'i').replace(/Ã³/g, 'o').replace(/Ãº/g, 'u').toLowerCase();
+var data = $(table).find('td');
+data.parent().show().removeClass('true'); 
+data.filter(':visible').each(function() {
+var text = $(this).text().replace(/Ã¡/g, 'a').replace(/Ã©/g, 'e').replace(/Ã­/g, 'i').replace(/Ã³/g, 'o').replace(/Ãº/g, 'u').toLowerCase();
+if(text.indexOf(val) > -1) {
+$(this).parent().addClass('true');
+} 
+});
+data.parent().not('.true').hide();
+});
+return this; 
+}; 
