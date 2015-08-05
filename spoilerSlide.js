0b6a80a2981b6dbcd57310f5ba0e4ddab22651ef
Deepfilter <jose@deepfilter.org>
2015-08-05 18:49:05 -0500

    Create spoilerSlide.js

---
+++ spoilerSlide.js
@@ -0,0 +1,17 @@
+$.fn.spoilerSlide = function() {
+var allPanels = $('dl dd').hide();
+$('dl dd:nth-of-type(1)').show().addClass('active');
+$('dl> dt > a').click(function() {
+$this = $(this);
+$target =  $this.parent().next();
+if(!$target.hasClass('active')){
+allPanels.removeClass('active').slideUp("fast");
+$target.addClass('active').slideDown("fast");
+var index = $this.parent().index();
+if(!$this.parent().index() == 0) { 
+$('html, body').stop().animate({scrollTop: 80 + index*25 - ((index-1)*2)}, 200); 
+} 
+}
+return false; 
+}); 
+};
